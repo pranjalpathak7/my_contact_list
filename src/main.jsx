e190d79723d90a1f209f8267ac3e063afddac16a
Pranjal Pathak <pranjalpathak876@gmail.com>
2025-10-24 23:08:43 +0530

    bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './App.css';
 // Note: We move App.css import to App.jsx
 
 ReactDOM.createRoot(document.getElementById('root')).render(
